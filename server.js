bd10746b0844921423fd96d0e1846342cbd80296
Mark Hazleton <mark.hazleton@controlorigins.com>
2020-10-03 15:41:35 -0500

    finish chapter 3

--- server.js
+++ server.js
@@ -2,19 +2,40 @@
 /* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session');
+
+const routes = require('./routes');
+
+const FeedbackService = require('./services/FeedbackService');
+const SpeakerService = require('./services/SpeakerService');
+
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakerService = new SpeakerService('./data/speakers.json');
 
 const app = express();
-
 const port = 3000;
+
+app.set('trust proxy', 1);
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['asdf98908adfasdf', 'adfdaf908asdfasdf'],
+  })
+);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
 
 app.use(express.static(path.join(__dirname, './static')));
 
-app.get('/', (_request, response) => {
-  response.sendFile(path.join(__dirname, './static/index.html'));
-});
-app.get('/speakers', (request, response) => {
-  response.sendFile(path.join(__dirname, './static/speakers.html'));
-});
+app.use(
+  '/',
+  routes({
+    feedbackService,
+    speakerService,
+  })
+);
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
